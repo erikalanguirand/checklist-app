42fd4364b55bb5a76bd72a68a19cc9212bacd519
Erika Languirand <erika@detroitlabs.com>
2018-09-03 23:39:09 -0400

    Adding AddItemViewController and connecting to view

---
+++ checklist-app/AddItemViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AddItemViewController.swift
+//  checklist-app
+//
+//  Created by Erika Carlson on 9/3/18.
+//  Copyright © 2018 Erika Carlson. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+}
